e3da30845f437e28c162311de83af1b5e5363738
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-09-22 13:25:42 +0800

    refactor: router.push -> router.replace
    使用replace代替push，防止当history.go(-1) 和按下浏览器返回键时，返回的又是前一个错误的页面
    
    Changes to be committed:
    	modified:   src/utils/index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,7 @@ import router from '../router'
  */
 export function handleError(err) {
     if (err.code == 404) {
-        router.push({ name: 404 });
+        //使用replace， 防止当history.go(-1) 和按下浏览器返回键时，返回的又是前一个错误的页面
+        router.replace({ name: 404 });
     }
 }
