0683d4a3d5b4390b7a3e0bae197c745a1656c461
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-09-22 18:03:29 +0800

    feature: add comicsitem component
    Changes to be committed:
    	new file:   src/components/ComicsItem.vue

---
+++ src/components/ComicsItem.vue
@@ -0,0 +1,39 @@
+<template>
+    <figure>
+        <router-link :to="'/ComicsView/'+id">
+            <img :src="coverImg" :alt="id">
+            <figcaption>
+                {{name}}
+            </figcaption>
+        </router-link>
+    </figure>
+</template>
+
+<script>
+export default {
+    props: {
+        id: {
+            type: String,
+            required: true,
+        },
+        coverImg: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+figure {
+    text-align: center;
+}
+
+figure img {
+    max-width: 100%;
+}
+</style>
