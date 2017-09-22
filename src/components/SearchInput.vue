9a80083864907882b877586d514d10e42d765723
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-09-22 17:44:06 +0800

    feature: add search input component
    Changes to be committed:
    	new file:   src/components/SearchInput.vue

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="input-group">
+        <input type="text" class="form-control" placeholder="请输入漫画名、作者等关键字">
+        <span class="input-group-btn">
+            <button class="btn btn-default" type="button">
+                <span class="glyphicon glyphicon-search"></span>
+            </button>
+        </span>
+    </div>
+</template>
+
