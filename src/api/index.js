7a076e92ba1d620083a7bf17dc8d618a5c1638b7
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-09-22 12:57:12 +0800

    feature: add api.getComicsChaptorInfo
    Changes to be committed:
    	modified:   src/api/index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,30 @@
 export default {
     /**
+     * 获取漫画某一章节的信息
+     * @param {String} comicsID
+     * @param {Number} chaptor
+     * @return {Promise}
+     * @todo 具体实现未完成
+     */
+    getComicsChaptorInfo(comicsID, chaptor) {
+        return new Promise((resolve, reject) => {
+            if (comicsID == 'commics1' && chaptor == 1) {
+                return resolve({
+                    title: '毛皮组',
+                    imgList: ['http://pic04.ishuhui.com/cartoon/book-1/1/878-9396/03.png?90530854',
+                        'http://pic04.ishuhui.com/cartoon/book-1/1/878-9396/03.png?90530854']
+                });
+            }
+            reject(new Error('没有该漫画'));
+        })
+    },
+    /**
      * 获取漫画图片的url
      * @param {String} comicsID
      * @param {Number} chaptor
      * @param {Number} page
      * @return {Promise}
+     * @todo 具体实现未完成
      */
     getComicsUrl(comicsID, chaptor, page) {
         return new Promise((resolve, reject) => {
