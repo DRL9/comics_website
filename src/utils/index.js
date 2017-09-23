f69008badf278b1805f0bb83f16f7d7c36711b1e
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-09-23 16:10:35 +0800

    feature: default error output to console
    Changes to be committed:
    	modified:   src/utils/index.js

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,5 +8,7 @@ export function handleError(err) {
     if (err.code == 404) {
         //使用replace， 防止当history.go(-1) 和按下浏览器返回键时，返回的又是前一个错误的页面
         router.replace({ name: 404 });
+    } else {
+        console.error(err);
     }
 }
