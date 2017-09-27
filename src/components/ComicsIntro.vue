d987322cce3bf3d54231b36ed813c8ce5f1afd8a
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-09-27 15:44:00 +0800

    feature: beautify router comicsIntro
    Changes to be committed:
    	modified:   src/components/ComicsIntro.vue

--- src/components/ComicsIntro.vue
+++ src/components/ComicsIntro.vue
@@ -1,24 +1,35 @@
 <template>
     <div class="comics-intro">
         <div class="container">
-            <search-input></search-input>
+            <div class="search">
+                <search-input></search-input>
+            </div>
         </div>
-
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-3">
-                    <img :src="coverImg" :alt="id">
-                </div>
-                <div class="col-sm-9">
-                    <h2>{{name}} </h2>
-                    <p>{{intro}}</p>
+        <div class="intro">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3">
+                        <img :src="coverImg" :alt="comicsId">
+                    </div>
+                    <div class="col-sm-9">
+                        <h2>{{name}} </h2>
+                        <ul>
+                            <li>作者：{{author}}</li>
+                            <li>总章节数：{{totalChaptor}}</li>
+                            <li>简介：{{intro}}</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
-            <ul>
-                <li v-for="chaptor of totalChaptor" :key="chaptor">
-                    <router-link :to="'/ComicsView/'+id+'/'+chaptor">{{chaptor}}</router-link>
-                </li>
-            </ul>
+        </div>
+        <div class="chaptors">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-3" v-for="(title,index) of chaptorTitles" :key="chaptor=index+1">
+                        <router-link :to="'/ComicsView/'+comicsId+'/'+chaptor">{{chaptor}} - {{title}}</router-link>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -35,27 +46,77 @@ export default {
     },
     data() {
         return {
-            id: '',
+            comicsId: '',
             intro: '',
             totalChaptor: 0,
             name: '',
-            coverImg: ''
+            author: '',
+            coverImg: '',
+            chaptorTitles: []
         }
     },
     created() {
-        this.setInfo();
+        this.comicsId = this.$route.params[routerParamsNames.comicsId];
+        this.fetchData();
     },
     methods: {
-        setInfo() {
-            this.id = this.$route.params[routerParamsNames.comicsId];
-            api.getComicsInfo(this.id).then((info) => {
-                this.coverImg = info.coverImg;
-                this.name = info.name;
-                this.intro = info.intro;
-                this.totalChaptor = info.totalChaptor;
+        fetchData() {
+            api.getComicsInfo(this.comicsId).then((info) => {
+                this.setInfo(info);
             }, handleError);
+        },
+        setInfo(info) {
+            this.coverImg = info.coverImg;
+            this.name = info.name;
+            this.intro = info.intro;
+            this.totalChaptor = info.totalChaptor;
+            this.author = info.author;
+            this.chaptorTitles = info.chaptorTitles;
         }
     }
 }
 </script>
 
+<style scoped>
+img {
+    max-width: 100%;
+    max-height: 15em;
+}
+
+.search {
+    margin: 1em 0;
+}
+
+.intro {
+    margin: 1em 0;
+}
+
+.intro img {
+    margin-bottom: 1em;
+}
+
+.intro h2 {
+    margin: 0 0 .4em;
+}
+
+.intro ul {
+    margin: 0 0 1em;
+    padding: 0;
+    list-style: none;
+    font-size: 1em;
+}
+
+.chaptors a {
+    display: block;
+    padding: .3em 1em;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    color: #000;
+}
+
+.chaptors a:hover {
+    color: skyblue;
+    border-color: skyblue;
+    text-decoration: none;
+}
+</style>
