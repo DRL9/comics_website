fbcaea22ef4c9695cc3e533da51056db5c55ba5f
Yuanjun Zhong <zhuimengzyj@gmail.com>
2017-10-11 10:46:11 +0800

    test: utils.handleError(err) can handle other err
    Changes to be committed:
    	modified:   test/specs/utils/index.spec.js

--- test/specs/utils/index.spec.js
+++ test/specs/utils/index.spec.js
@@ -5,7 +5,7 @@ import { handleError } from '@/utils/index.js'
 
 describe('utils/', () => {
     describe('index.js', () => {
-        it('handleError(err)', (done) => {
+        it('handleError(err) can handle 404 error', (done) => {
             var error404 = new Error('');
             error404.code = 404;
             handleError(error404);
@@ -13,6 +13,10 @@ describe('utils/', () => {
                 expect(location.href).to.be.contain('/notFound')
                 done();
             });
+        });
+        it('handleError(err) can handle other err', () => {
+            var err = new Error('');
+            handleError(err);
         });
     });
 });
